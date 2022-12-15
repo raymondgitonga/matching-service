f83ea3f7d5616fdbec06e69814fdc2631510a3ce
raymondgitonga <raytosh95@gmail.com>
2022-12-15 13:16:36 +0000

    update tests

--- matching-client/internal/adapters/httpclient/client_test.go
+++ matching-client/internal/adapters/httpclient/client_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestMatchingClient_GetPartner(t *testing.T) {
 
-	t.Run("Successful partner call", func(t *testing.T) {
+	t.Run("Successful matching call, partner returned", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			rw.WriteHeader(http.StatusOK)
 			_, err := rw.Write([]byte(`{
@@ -42,13 +42,34 @@ func TestMatchingClient_GetPartner(t *testing.T) {
 		assert.Equal(t, "Cummerata, Wolff and Hauck", partner.Result[0].Name)
 	})
 
-	t.Run("Failed partner call", func(t *testing.T) {
+	t.Run("Successful matching call, no partner returned", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.WriteHeader(http.StatusOK)
+			_, err := rw.Write([]byte(`{
+							"result": [],
+							"error": false,
+							"message": "success"
+						}`))
+			assert.NoError(t, err)
+		}))
+
+		defer server.Close()
+
+		config := httpclient.NewConfig(server.URL)
+		matchingClient := httpclient.NewMatchingClient(*server.Client(), *config)
+		partner, err := matchingClient.GetPartner("1")
+
+		assert.NoError(t, err)
+		assert.Equal(t, 0, len(partner.Result))
+	})
+
+	t.Run("Matching request BadRequest", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			rw.WriteHeader(http.StatusBadRequest)
 			_, err := rw.Write([]byte(`{
 							"result": [],
 							"error": true,
-							"message": "no partner found"
+							"message": "malformed request"
 						}`))
 			assert.NoError(t, err)
 		}))
@@ -63,7 +84,7 @@ func TestMatchingClient_GetPartner(t *testing.T) {
 		assert.Nil(t, partner)
 	})
 
-	t.Run("Matching service fails to respond", func(t *testing.T) {
+	t.Run("Matching request InternalServerError", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 			rw.WriteHeader(http.StatusInternalServerError)
 			_, err := rw.Write([]byte(``))
